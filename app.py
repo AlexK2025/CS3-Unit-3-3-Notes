0aaaa735aea405dcf107d129f67d1073ffb5acc4
Alex <125302863+alexky0@users.noreply.github.com>
2025-02-24 08:50:28 -0500

    created!!

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import  Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.now(datetime.timezone.utc))
